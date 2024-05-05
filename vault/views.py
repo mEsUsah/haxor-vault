af7457a045e1a63492ecc5d636dd4f08cdf7addd
Stanley Skarshaug <stanley@haxor.no>
2024-05-05 22:46:11 +0200

    fix: add dashboard url and view

--- vault/views.py
+++ vault/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="login")
+def dashboard(request):
+    context = {
+    }
+    return render(request, 'vault/dashboard.html', context)
