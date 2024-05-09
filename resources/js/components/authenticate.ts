77493d9cbadf40f2d75e725e1712f7806e6ae300
Stanley Skarshaug <stanley@haxor.no>
2024-05-09 18:38:58 +0200

    bug(login): save plaintext password in localstorage

--- resources/js/components/authenticate.ts
+++ resources/js/components/authenticate.ts
@@ -10,11 +10,19 @@ interface AuthenticationInterface {
 const url = "/login";
 
 /**
- * @param {AuthenticationInterface} data - Data that will be passed to the API
- * @returns {Promise<string>} Promise with string
+ * Authenticates using front-end password hashing, and passes the hash
+ * to the backend endpoint.
+ * 
+ * If successfully authenticated, the session cookie will be set, and 
+ * cleartext password will be stored in localstorage. Password is stored in 
+ * localstorage for ease of encrypting and decrypting secrets once authenticated.
+ * 
+ * @param {AuthenticationInterface} data - Data that will be passed to the API.
+ * @returns {Promise<string>} Promise with status string.
  */
 export async function authenticate(data: AuthenticationInterface): Promise<string> {
-    let passwordHash: string = await generateHash(data.password);
+    let passwordClearText = data.password;
+    let passwordHash: string = await generateHash(passwordClearText);
     data.password = passwordHash;
     
     return new Promise((resolve, reject) => {
@@ -27,7 +35,7 @@ export async function authenticate(data: AuthenticationInterface): Promise<strin
         .then((response)=>{
             if(response.status == 200){
                 if(response.data.authenticated){
-                    localStorage.setItem("password", passwordHash);
+                    localStorage.setItem("password", passwordClearText);
                     resolve("success");
                 } else {
                     localStorage.removeItem("password");
