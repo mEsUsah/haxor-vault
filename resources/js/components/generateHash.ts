2f99be79a601dfd05f68b8c8095084ac62c188ea
Stanley Skarshaug <stanley@haxor.no>
2024-05-09 18:13:15 +0200

    fix(generateHash): add jsDoc

--- resources/js/components/generateHash.ts
+++ resources/js/components/generateHash.ts
@@ -1,6 +1,11 @@
 import argon2 from "argon2-browser/dist/argon2-bundled.min.js";
 
-export function generateHash(password): Promise<string> {
+/**
+ * 
+ * @param {string} password - plaintext password
+ * @returns {Promise<string>} Promise with hashed password
+ */
+export function generateHash(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
         argon2.hash({ pass: password, salt: 'somesalt' })
             .then(hash => {
