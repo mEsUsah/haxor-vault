c548024afcea803b8642c480a755ac5659f76b91
Stanley Skarshaug <stanley@haxor.no>
2024-05-09 18:39:35 +0200

    doc(generateHash): ad jsDoc

--- resources/js/components/generateHash.ts
+++ resources/js/components/generateHash.ts
@@ -1,13 +1,16 @@
 import argon2 from "argon2-browser/dist/argon2-bundled.min.js";
 
+const salt = 'hackingIsSimplyCuriosity';
+
 /**
+ * Generate Argon2 hash of supplied password.
  * 
- * @param {string} password - plaintext password
- * @returns {Promise<string>} Promise with hashed password
+ * @param {string} password - plaintext password that is to be hashed.
+ * @returns {Promise<string>} Promise with hash string.
  */
 export function generateHash(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
-        argon2.hash({ pass: password, salt: 'somesalt' })
+        argon2.hash({ pass: password, salt: salt })
             .then(hash => {
                 resolve(hash.encoded);
             })
