fec0a87d5e2b4dc9c29fb7a81080fc5db16019d0
Stanley Skarshaug <stanley@haxor.no>
2024-05-09 17:43:19 +0200

    fix(login): refactor authentation into separate function

--- resources/js/Login.vue
+++ resources/js/Login.vue
@@ -50,50 +50,65 @@ export default defineComponent({
         const password = ref(<string>"");
         const statusMessage = ref(<StatusMessage|null>null);
 
-        
+        async function authenticate(data: object): Promise<string> {
+            return new Promise((resolve, reject) => {
+                axios.post("/login", data, {
+                    withCredentials: true,
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then((response)=>{
+                    if(response.status == 200){
+                        if(response.data.authenticated){
+                            localStorage.setItem("password", password.value);
+                            resolve("success");
+                        } else {
+                            localStorage.removeItem("password");
+                            reject("password-error");
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                    reject("system-error");
+                });
+
+            })
+        }
 
         async function login(event:Event): Promise<void> {
             event.preventDefault();
             let passwordHash: string = await generateHash(password.value);
-
-            axios.post("/login", {
+            
+ 
+            await authenticate({
                 username: username.value,
                 password: passwordHash,
                 csrfmiddlewaretoken: csrfToken
-            }, {
-                withCredentials: true,
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-            .then((response)=>{
-                if(response.status == 200){
-                    if(response.data.authenticated){
-                        localStorage.setItem("password", password.value);
-                        statusMessage.value = {
-                            type: "success",
-                            messageClass: "alert--success",
-                            text: "Logged in!",
-                        };
-                    } else {
-                        statusMessage.value = {
-                            type: "error",
-                            messageClass: "alert--error",
-                            text: "Username or Password was wrong",
-                        };
-                        animateElementShake("loginAlert");
-                        localStorage.removeItem("password");
-                    }
-                }
-            })
-            .catch(error => {
+            }).then(() => {
                 statusMessage.value = {
-                    type: "error",
-                    messageClass: "alert--error",
-                    text: "Sorry, we have a problem...",
+                    type: "success",
+                    messageClass: "alert--success",
+                    text: "Logged in!",
                 };
-                animateElementShake("loginAlert");
-                location.reload(); // Refresh page to get new csrf token
+            }).catch(error => {
+                if(error == "password-error"){
+                    statusMessage.value = {
+                        type: "error",
+                        messageClass: "alert--error",
+                        text: "Username or Password was wrong",
+                    }
+                    animateElementShake("loginAlert");
+                }
+                if(error == "system-error"){
+                    statusMessage.value = {
+                        type: "error",
+                        messageClass: "alert--error",
+                        text: "Sorry, we have a problem...",
+                    };
+                    location.reload(); // Refresh page to get new csrf token
+                }
             });
         }
 
