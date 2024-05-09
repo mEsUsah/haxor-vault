1d4667a3d656124bbf3fe1553b2279c0e3284f1e
Stanley Skarshaug <stanley@haxor.no>
2024-05-09 20:46:47 +0200

    fix(vault): remove apptype from admin

--- vault/admin.py
+++ vault/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import AppType, App, Credential
 
 # Register your models here.
-admin.site.register(AppType)
 admin.site.register(App)
 admin.site.register(Credential)
