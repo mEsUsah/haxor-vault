e22ea607b7b28710777ab3d0f3a80ce5f03c242f
Stanley Skarshaug <stanley@haxor.no>
2024-05-10 15:23:32 +0200

    fix: add app list boilerplate

---
+++ resources/js/AppList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>hello apps</p>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+</script>
--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,2 +1,8 @@
-console.log("allive");
-console.log("allive!");
+import { createApp } from 'vue';
+import AppList from './AppList.vue';
+
+const appListMountPoint = document.getElementById("vueAppsList");
+if(appListMountPoint){
+    const appList = createApp(AppList)
+    appList.mount(appListMountPoint);
+}
