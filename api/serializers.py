f3700d3686c90b52dc8cb1a0ed8ea81ef4373f4a
Stanley Skarshaug <stanley@haxor.no>
2024-05-10 14:52:49 +0200

    add serializers to show relationships

--- api/serializers.py
+++ api/serializers.py
@@ -5,13 +5,13 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','username']
 
 
 class AppTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppType
-        fields = '__all__'
+        fields = ['id','name']
 
 
 class AppSerializer(serializers.ModelSerializer):
@@ -19,7 +19,34 @@ class AppSerializer(serializers.ModelSerializer):
         model = App
         fields = ['id','name', 'apptype','user']
 
+
+class AppsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = ['id','name', 'apptype','user','credentials']
+
+    credentials = serializers.SerializerMethodField()
+
+    def get_credentials(self, object):
+        credentials = object.credential_set.all()
+        serializer = CredentialSerializer(credentials, many=True)
+        return serializer.data
+
+
 class CredentialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Credential
-        fields = '__all__'
+        fields = ['id','app','username','password']
+
+
+class CredentialsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Credential
+        fields = ['id','app','username','password']
+
+    app = serializers.SerializerMethodField()
+
+    def get_app(self, object):
+        app = object.app
+        serializer = AppSerializer(app)
+        return serializer.data
