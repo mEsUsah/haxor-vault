5decb18566c32a5a6d9c68a1646310b3e53f7480
Stanley Skarshaug <stanley@haxor.no>
2024-05-11 20:08:04 +0200

    fix(config): add apptype API endpoint

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -2,5 +2,6 @@ const s3path: string = "https://devsearch-bucket.eu-central-1.linodeobjects.com/
 export const staticPath:string = process.env.NODE_ENV === "production" ? s3path : "/static/";
 export const api_host = "http://127.0.0.1:8000";
 export const api_apps = "/api/v1/apps";
+export const api_apptypes = "/api/v1/apptypes";
 
 console.log('staticPath:', staticPath);
