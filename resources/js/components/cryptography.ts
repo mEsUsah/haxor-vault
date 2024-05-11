865c65f60cc622968f9014bbeb8d246ccaba87e2
Stanley Skarshaug <stanley@haxor.no>
2024-05-11 13:34:17 +0200

    doc: add jsdoc to crypto functions

--- resources/js/components/cryptography.ts
+++ resources/js/components/cryptography.ts
@@ -1,3 +1,9 @@
+/**
+ * A set of simple and easy-to-use cryptography functions.
+ * @author Stanley Skarshaug
+ * @requires argon2-browser":^1.18.0
+ * @requires crypto-js":^4.2.0
+ */
 import argon2 from "argon2-browser/dist/argon2-bundled.min.js";
 import CryptoJS  from 'crypto-js';
 
@@ -5,7 +11,6 @@ const salt = 'hackingIsSimplyCuriosity';
 
 /**
  * Generate Argon2 hash of supplied password.
- * 
  * @param {string} password - plaintext password that is to be hashed.
  * @returns {Promise<string>} Promise with hash string.
  */
@@ -22,10 +27,22 @@ export function hashArgon2(password: string): Promise<string> {
     });
 };
 
+/**
+ * Encrypt plaintext using AES.
+ * @param {string} plaintext - plaintex to be encrypted
+ * @param {string} key - encryption key, used for the encryption
+ * @returns {string} ciphertext
+ */
 export function encryptAES(plaintext: string, key: string): string{
     return CryptoJS.AES.encrypt(plaintext, key).toString();
 }
 
+/**
+ * Decryptes ciphertext using AES.
+ * @param {string} ciphertext - ciphertext to be decrypted
+ * @param {string} key - encryption key, used for the encryption
+ * @returns {string} plaintext
+ */
 export function decryptAES(ciphertext: string, key: string): string{
     var bytes  = CryptoJS.AES.decrypt(ciphertext, key);
     return bytes.toString(CryptoJS.enc.Utf8);
