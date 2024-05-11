6eb4ee0df4bc8ea8d36a852adafd9689cf927645
Stanley Skarshaug <stanley@haxor.no>
2024-05-11 22:29:51 +0200

    fix: add basic createApp function

--- resources/js/components/appCRUD.ts
+++ resources/js/components/appCRUD.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { api_host, api_apps } from '../config.ts';
-import { App } from './interfaces.ts';
+import { App, AppSchema } from './interfaces.ts';
 
 export async function getApps(): Promise<App[]>{
     return new Promise((resolve, reject) => {
@@ -12,5 +12,24 @@ export async function getApps(): Promise<App[]>{
             }).catch(error => {
                 reject('Failed to get apps from the API');
             });
+    }); 
+}
+
+export async function createApp(data: AppSchema): Promise<App>{
+    return new Promise((resolve, reject) => {
+        const url = api_host + api_apps;
+
+        axios.post(url, data, {
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(response => {
+            if(response.status == 201){
+                resolve(response.data);
+            }
+        }).catch(error => {
+            reject('Failed create app');
+        });
     }); 
 }
