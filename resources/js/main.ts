9ae72192c643af212612b671bae12ccc97821732
Stanley Skarshaug <stanley@haxor.no>
2024-05-11 12:22:05 +0200

    fix: add app form boilerplate

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue';
 import AppList from './AppList.vue';
+import AppForm from './AppForm.vue';
 
 const appListMountPoint = document.getElementById("vueAppsList");
 if(appListMountPoint){
     const appList = createApp(AppList)
     appList.mount(appListMountPoint);
+}
+
+const appFormMountPoint = document.getElementById("vueAppForm");
+if(appFormMountPoint){
+    const appForm = createApp(AppForm)
+    appForm.mount(appFormMountPoint);
 }
