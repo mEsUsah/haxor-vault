25380191e430a71c86c3874466cd9c41f4029d02
Stanley Skarshaug <stanley@haxor.no>
2024-05-11 22:30:37 +0200

    fix(create-app): implement craeteApp functionality

--- resources/js/AppForm.vue
+++ resources/js/AppForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="form__wrapper">
         <div class="form__field">
-            <select name="apptype" v-model="selectedApptype">
+            <select name="apptype" v-model="app.apptype">
                 <option value="" disabled>Select type:</option>
                 <option v-for="appType in appTypes" :value="appType.id">
                     {{ appType.name }}
@@ -9,25 +9,34 @@
             </select>
         </div>
         <div class="form__field">
-            <input v-model="selectedName" type="text" name="name" placeholder="Name">
+            <input v-model="app.name" type="text" name="name" placeholder="Name">
         </div>
         <div class="form__buttons">
-            <button :disabled="disableSubmit" type="submit" class="button">Save</button>
+            <button 
+                :disabled="disableSubmit"
+                @click="saveApp"
+                type="submit" class="button">Save</button>
+            <a href="/dashboard" class="button button--nautral">Cancel</a>
         </div>
     </div>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, ref, computed } from 'vue';
-import { AppType } from './components/interfaces.ts';
+import { defineComponent, onMounted, ref, reactive, computed } from 'vue';
+import { AppType, AppSchema } from './components/interfaces.ts';
 import { getAppTypes } from './components/appTypeCRUD.ts';
+import { createApp } from './components/appCRUD.ts';
 
 export default defineComponent({
     setup() {
-        const selectedName = ref<string>("");
-        const selectedApptype = ref<string>("");
+        const app: AppSchema = reactive({
+            name: "",
+            apptype: "",
+            csrfmiddlewaretoken: csrfToken
+        });
+        
         const appTypes = ref<AppType[]>();
         const disableSubmit = computed(()=>{
-            return selectedName.value == "" || selectedApptype.value == "";
+            return app.name == "" || app.apptype == "";
         })
         
         async function getData(): Promise<void>{
@@ -35,15 +44,27 @@ export default defineComponent({
             console.log(appTypes.value);
         }
 
+        function saveApp(){
+            if(!disableSubmit.value){
+                createApp(app)
+                    .then(result => {
+                        console.log(result);
+                    })
+                    .catch(error=>{
+                        console.log(error);
+                    });
+            }
+        }
+
         onMounted(() => {
             getData(); 
         });
 
         return {
-            selectedName,
-            selectedApptype,
+            app,
             appTypes,
-            disableSubmit
+            disableSubmit,
+            saveApp,
         }
     }
 });
