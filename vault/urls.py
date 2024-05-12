e4873b6beb80df3f144270e0e9af631f38fe29c8
Stanley Skarshaug <stanley@haxor.no>
2024-05-12 13:55:16 +0200

    fix(credentials): add url and view

--- vault/urls.py
+++ vault/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('dashboard', views.dashboard, name="dashboard"),
     path('app/create', views.create_app, name="create-app"),
     path('app/<str:id>', views.edit_app, name="edit-app"),
+    path('credential/create', views.create_credential, name="create-credential"),
+    path('credential/<str:id>', views.edit_credential, name="edit-credential"),
 ]
--- vault/views.py
+++ vault/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from vault.models import App
+from vault.models import App, Credential
 
 # Create your views here.
 @login_required(login_url="login")
@@ -20,3 +20,16 @@ def edit_app(request, id):
         'app': app
     }
     return render(request, 'vault/app_details.html', context)
+
+@login_required(login_url="login")
+def create_credential(request):
+    context = {}
+    return render(request, 'vault/credential_create.html', context)
+
+@login_required(login_url="login")
+def edit_credential(request, id):
+    app = Credential.objects.get(id=id)
+    context = {
+        'app': app
+    }
+    return render(request, 'vault/credential_details.html', context)
