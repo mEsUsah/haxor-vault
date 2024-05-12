cea70f4b5d43c62b556012ca46e2c1c0516c53bb
Stanley Skarshaug <stanley@haxor.no>
2024-05-12 14:05:06 +0200

    fix(config): add api_credentials route

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -3,5 +3,6 @@ export const staticPath:string = process.env.NODE_ENV === "production" ? s3path 
 export const api_host = "http://127.0.0.1:8000";
 export const api_apps = "/api/v1/apps";
 export const api_apptypes = "/api/v1/apptypes";
+export const api_credentials = "/api/v1/credentials";
 
 console.log('staticPath:', staticPath);
