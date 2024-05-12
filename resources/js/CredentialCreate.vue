e42489d93c4b124f7d529d2970e7659666c42d6b
Stanley Skarshaug <stanley@haxor.no>
2024-05-12 14:18:45 +0200

    fix: add CredentialCreate app

---
+++ resources/js/CredentialCreate.vue
@@ -0,0 +1,47 @@
+<template>
+    <CredentialForm
+        :newCredential="true"
+        :apps="apps"
+        @saveCredential="saveCredential"
+    ></CredentialForm>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref, reactive, computed } from 'vue';
+import { CredentialSchema, Credential, AppType } from './components/interfaces.ts';
+import { getApps } from './components/appCRUD.ts';
+import { createCredential } from './components/credentialCRUD.ts';
+import CredentialForm from './views/CredentialForm.vue';
+
+export default defineComponent({
+    components: {
+        CredentialForm,
+    },
+    setup() {      
+        const apps = ref<AppType[]>();
+        
+        async function getData(): Promise<void>{
+            apps.value = await getApps();
+        }
+
+        function saveCredential(credential: CredentialSchema){
+            createCredential(credential)
+                .then((result: Credential) => {
+                    console.log(result);
+                    // window.location.href = "/credential/" + result.id;
+                })
+                .catch(error=>{
+                    console.log(error);
+                });
+        }
+
+        onMounted(() => {
+            getData(); 
+        });
+
+        return {
+            apps,
+            saveCredential,
+        }
+    }
+});
+</script>
--- resources/js/main.ts
+++ resources/js/main.ts
@@ -2,21 +2,31 @@ import { createApp } from 'vue';
 import AppList from './AppList.vue';
 import AppDetails from './AppDetails.vue';
 import CreateApp from './AppCreate.vue';
+import CreateCredential from './CredentialCreate.vue';
 
+// Dashboard
 const appListMountPoint = document.getElementById("vueAppsList");
 if(appListMountPoint){
-    const appList = createApp(AppList)
+    const appList = createApp(AppList);
     appList.mount(appListMountPoint);
 }
 
+// Apps
 const appCreateMountPoint = document.getElementById("vueAppCreate");
 if(appCreateMountPoint){
-    const appForm = createApp(CreateApp)
+    const appForm = createApp(CreateApp);
     appForm.mount(appCreateMountPoint);
 }
 
 const appDetailsMountPoint = document.getElementById("vueAppDetails");
 if(appDetailsMountPoint){
-    const appForm = createApp(AppDetails)
+    const appForm = createApp(AppDetails);
     appForm.mount(appDetailsMountPoint);
+}
+
+// Credentials
+const credentialCreateMountPoint = document.getElementById("vueCredentialCreate");
+if(credentialCreateMountPoint){
+    const credentialForm = createApp(CreateCredential);
+    credentialForm.mount(credentialCreateMountPoint);
 }
