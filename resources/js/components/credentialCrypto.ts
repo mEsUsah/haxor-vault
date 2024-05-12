98e1f91debb8f5cd003966c79e2a60c57812f37d
Stanley Skarshaug <stanley@haxor.no>
2024-05-12 14:16:17 +0200

    fix(credentialCrypto): add decryptCredential

--- resources/js/components/credentialCrypto.ts
+++ resources/js/components/credentialCrypto.ts
@@ -2,7 +2,21 @@ import { Credential, CredentialSchema } from './interfaces.ts';
 import { encryptAES, decryptAES } from './cryptography.ts';
 
 /**
- * Encrypts an AppSchema.
+ * Decrypts credential, and app it belongs to.
+ * @param { Credential } credential - Array of Apps with encrypted name and credentials
+ * @param {string} key - Decryption key
+ * @returns { Credential } Decrypted Apps
+ */
+export function decryptCredential(credential:Credential, key: string): Credential {
+    credential.username = decryptAES(credential.username, key);
+    credential.password = decryptAES(credential.password, key);
+    credential.app.name = decryptAES(credential.app.name, key);
+    return credential;
+}
+
+
+/**
+ * Encrypts an CredentialSchema.
  * @param { CredentialSchema } credential - Plaintext credential schema
  * @param {string} key - Encryption key
  * @returns { CredentialSchema } Encrypted credential schema
