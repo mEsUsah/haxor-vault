5e673f8881b1c12d08097b65367e26e7170ba29a
Stanley Skarshaug <stanley@haxor.no>
2024-05-12 11:30:27 +0200

    fix(appDetails): implement update functionality

--- resources/js/AppDetails.vue
+++ resources/js/AppDetails.vue
@@ -1,18 +1,58 @@
 <template>
     <section>
         <div class="section__hedline">
-            <h1>allive</h1>
+            <h1>{{ app?.name }}</h1>
         </div>
         <hr>
-        <div ></div>
+        <AppForm
+            :app="app"
+            :appTypes="appTypes"
+            @saveApp="saveApp">
+        </AppForm>
+        
     </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, reactive, computed } from 'vue';
-export default defineComponent({
-    setup() {
+import { getAppTypes } from './components/appTypeCRUD.ts';
+import { getApp, updateApp } from './components/appCRUD.ts'
+import { App, AppType, AppSchema } from './components/interfaces.ts';
+import AppForm from './views/AppForm.vue'
 
+export default defineComponent({
+    components: {
+        AppForm
+    },
+    setup() {
+        const app = ref<App>()
+        const appTypes = ref<AppType[]>();
+        
+        async function getData(){
+            app.value = await getApp(appId);
+            console.log(app);
+            appTypes.value = await getAppTypes();
+        }
+
+        function saveApp(appSchema: AppSchema){
+            updateApp(appId, appSchema)
+                .then((result:App) => {
+                    app.value = result;
+                })
+                .catch(error=>{
+                    console.log(error);
+                });
+        }
+
+        onMounted(()=>{
+            getData();
+        });
+
+        return {
+            app,
+            appTypes,
+            saveApp
+        }
     }
 })
 </script>
