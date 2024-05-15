d565180e9a5f511a261e1e4818997c7b5439bae6
Stanley Skarshaug <stanley@haxor.no>
2024-05-15 22:56:06 +0200

    fix(register) add basic register user functnionality

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -4,7 +4,7 @@
             v-if="statusMessage"
             :statusMessage>
         </StatusMessageBox>
-        <form class="login-form__wrapper">
+        <form @submit="register" class="login-form__wrapper">
             <img class="login-section__logo" 
                 :src="assets.haxorLogo" 
                 alt="logo">
@@ -24,7 +24,7 @@
                     v-model="passwordConfirm"
                     placeholder="Confirm password">
             </div>
-            <button type="submit">Register</button> 
+            <button type="submit" :disabled="!enableRegistertration">Register</button> 
             <div>Already have an account? <a href="/login">Login</a></div>
         </form>
     </div>
@@ -35,7 +35,10 @@ import { defineComponent, reactive, ref } from 'vue';
 import StatusMessageBox from './views/MessageBox.vue';
 import { staticPath } from './config';
 import { animateElementShake } from './components/animations';
-import {StatusMessage} from './components/interfaces.ts';
+import {RegistrationSchema, StatusMessage} from './components/interfaces.ts';
+import { validateRegistrationSchema } from './components/validateRegistrationSchema.ts';
+import { registerUser } from './components/userRegister.ts';
+const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 
 export default defineComponent({
     components: {
@@ -49,13 +52,53 @@ export default defineComponent({
         const password = ref(<string>"");
         const passwordConfirm = ref(<string>"");
         const statusMessage = ref(<StatusMessage|null>null);
+        const enableRegistertration = ref(<boolean>true);
+
+        function register(event:Event): void {
+            event.preventDefault();
+            
+            const registrationSchema: RegistrationSchema = {
+                email: email.value,
+                password: password.value,
+                passwordConfirm: passwordConfirm.value,
+                csrfmiddlewaretoken: csrfToken?csrfToken:"",
+            };
+            const validationResult = validateRegistrationSchema(registrationSchema);
+            if (!validationResult.success){
+                statusMessage.value = validationResult.message;
+                animateElementShake("loginAlert");
+                return;
+            }
+
+            registerUser(registrationSchema)
+                .then(data => {
+                    statusMessage.value = {
+                        type: "success",
+                        messageClass: "message--success",
+                        text: "Registered! Check your Email to verify your account",
+                    };
+                    enableRegistertration.value = false;
+                })
+                .catch(error => {
+                    if(error == "user-exists"){
+                        statusMessage.value = {
+                            type: "error",
+                            messageClass: "message--error",
+                            text: "User already exists!",
+                        };
+                        animateElementShake("loginAlert");
+                    }
+                });
+        }
 
         return {
             assets,
             email,
             password,
             passwordConfirm,
+            enableRegistertration,
             statusMessage,
+            register,
         }
     }
 });
