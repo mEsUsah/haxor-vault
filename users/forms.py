c5316e8d4b4b60b417ca43813ae1921dc5ae092b
Stanley Skarshaug <stanley@haxor.no>
2024-05-15 22:49:19 +0200

    fix: add UserForm

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+
+from users.models import User
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'email',
+        ]
