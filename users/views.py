a478dd57846b5840549a23f34cf1ef721a8f6d0a
Stanley Skarshaug <stanley@haxor.no>
2024-05-15 22:50:20 +0200

    fix(users): add create user logic

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.http import JsonResponse
+from users.models import User
+from users.forms import UserForm
 
 def login_user(request):
     if request.method == 'POST':
@@ -29,6 +31,43 @@ def login_user(request):
 def register_user(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
+    
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        passwordConfirm = request.POST['passwordConfirm']
+
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({
+                'message': "User already exists",
+                "registered": False,
+                "error": "user-exists"
+            }, status=400)
+        
+        if password != passwordConfirm:
+            return JsonResponse({
+                'message': "Passwords do not match",
+                "registered": False,
+                "error": "password-mismatch"
+            }, status=400)
+            
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.email
+            user.password_hash = password
+            user.is_active = 0
+            user.save()
+
+            return JsonResponse({
+                "message": "successfully registered user",
+                "registered": True
+            }, status=200)
+        else:
+            return JsonResponse({
+                'message': "validation error",
+                'errors': form.errors, 
+            }, status=400)
 
     return render(request, 'users/register.html')
 
