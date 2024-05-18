88d467ac951591cf319fa6be169fd306e72e715c
Stanley Skarshaug <stanley@haxor.no>
2024-05-18 22:30:55 +0200

    fix(app-form): add frontend validation

--- resources/js/components/validateAppSchema.ts
+++ resources/js/components/validateAppSchema.ts
@@ -1,4 +1,4 @@
-import { AppType, App, AppSchema } from '../components/interfaces.ts';
+import { AppSchema } from '../components/interfaces.ts';
 
 export function validateAppSchema(app: AppSchema): Boolean {
     if (app.name.trim() === "" || app.name.length > 100) {
