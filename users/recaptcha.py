28691a625281998f110cf75f7dea9599f5fdfb68
Stanley Skarshaug <stanley@haxor.no>
2024-05-18 13:09:56 +0200

    fix: add recaptcha module

---
+++ users/recaptcha.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+def validate_captcha_token(token):
+    secret_key = os.getenv("CAPTCHA_SECRET_KEY")
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={
+        'secret': secret_key,
+        'response': token
+    }).json()
+    
+    if response['success'] and response['score'] >= 0.5:
+        return True
+    return False
