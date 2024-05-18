269c264a27720ca227cada21ce0014781781082b
Stanley Skarshaug <stanley@haxor.no>
2024-05-18 20:02:55 +0200

    fix(config): remove definded api hostname

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -1,6 +1,6 @@
 const s3path: string = "https://devsearch-bucket.eu-central-1.linodeobjects.com/vault.haxor.no/static/";
 export const staticPath:string = process.env.NODE_ENV === "production" ? s3path : "/static/";
-export const api_host = "https://vault.haxor.no";
+export const api_host = "";
 export const api_apps = "/api/v1/apps";
 export const api_apptypes = "/api/v1/apptypes";
 export const api_credentials = "/api/v1/credentials";
