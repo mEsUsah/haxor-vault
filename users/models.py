afff431858616f394080dd8d7a7aa72aeae2f1d3
Stanley Skarshaug <stanley@haxor.no>
2024-05-18 14:10:54 +0200

    fix(users): add verification_code field

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     password_hash = models.CharField(max_length=200, blank=True, null=True)
+    verification_code = models.UUIDField(
+        default=uuid.uuid4, 
+        unique=True)
