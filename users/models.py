5c756613722eb0769a8c2bbadf88d15d36204119
Stanley Skarshaug <stanley@haxor.no>
2024-05-19 14:21:04 +0200

    doc(User model): add docstring

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """Custom user model that replaces the default User model."""
+    
     password_hash = models.CharField(max_length=200, blank=True, null=True)
     verification_code = models.UUIDField(
         default=uuid.uuid4, 
