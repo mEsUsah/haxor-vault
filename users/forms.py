50f70cbfaacb035a3bdc7314f515ad1abd615a46
Stanley Skarshaug <stanley@haxor.no>
2024-05-19 14:21:34 +0200

    doc(User form): add docstring

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from users.models import User
 
 class UserForm(ModelForm):
+    """Form for the User model."""
+    
     class Meta:
         model = User
         fields = [
