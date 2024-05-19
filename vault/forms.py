5c6955a24b8c06af6a37b8fde847908f883bf8d2
Stanley Skarshaug <stanley@haxor.no>
2024-05-19 14:32:06 +0200

    doc(vault): add docstrings to forms

--- vault/forms.py
+++ vault/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from vault.models import App, Credential
 
 class AppForm(ModelForm):
+    """Form for the App model."""
+    
     class Meta:
         model = App
         fields = [
@@ -11,6 +13,8 @@ class AppForm(ModelForm):
         ]
 
 class CredentialForm(ModelForm):
+    """Form for the Credential model."""
+    
     class Meta:
         model = Credential
         fields = [
