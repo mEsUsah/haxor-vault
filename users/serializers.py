c5304b2503909c3a9aa9240842b24b4854c667be
Stanley Skarshaug <stanley@haxor.no>
2024-05-19 14:18:42 +0200

    doc(userSerializer): add docstring

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the User model."""
+
     class Meta:
         model = User
         fields = ['id','username']
