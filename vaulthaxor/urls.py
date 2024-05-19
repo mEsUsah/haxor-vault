6fa7f2cbc285ed9c9fcd80d1146d8d8ecd30862c
Stanley Skarshaug <stanley@haxor.no>
2024-05-19 12:00:36 +0200

    fix: disable admin in prod

--- vaulthaxor/urls.py
+++ vaulthaxor/urls.py
@@ -1,26 +1,20 @@
-"""
-URL configuration for vaulthaxor project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+DEBUG = os.getenv("DEBUG", 'False').lower() in ('true', '1', 't')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('jwt/', include('jwtauth.urls')),
     path('api/', include('api.urls')),
     path('', include('users.urls')),
     path('', include('vault.urls')),
 ]
+
+# Only enable the admin interface in DEBUG mode
+if DEBUG:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ] + urlpatterns
