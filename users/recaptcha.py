32a1031cf9d2457ffba73ecdb6d7d7fa7815badf
mEsUsah <stanley@haxor.no>
2025-10-07 17:11:08 +0100

    fix(recaptcha): log validation errors

--- users/recaptcha.py
+++ users/recaptcha.py
@@ -1,15 +1,21 @@
 import requests
 import os
+import logging
+
 
 def validate_captcha_token(token: str) -> bool:
     """Validate the captcha token using Google's reCAPTCHA API."""
     
+    logger = logging.getLogger('errorlogger')
     secret_key = os.getenv("CAPTCHA_SECRET_KEY")
     response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={
         'secret': secret_key,
         'response': token
     }).json()
-    
+
     if response['success'] and response['score'] >= 0.5:
         return True
+    else:
+        logger.error(f"validation failed with error codes: {response['error-codes']}")
+
     return False
