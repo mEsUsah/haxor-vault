1d28f360b2b4c107cecd1db9734ff116a65615ac
mEsUsah <stanley@haxor.no>
2025-10-07 17:26:25 +0100

    fix(homepage): redirect to dashboard if authenticated

--- vault/views/articles.py
+++ vault/views/articles.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
     context = {}
     return render(request, 'vault/home.html', context)
